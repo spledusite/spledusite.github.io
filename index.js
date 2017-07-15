c23806d5cc993b20ad9c87a54edd5897f25b01a8
spledusite <spledusite@gmail.com>
2017-07-16 07:11:07 +1000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+var message;
+message = "Our Login/Signup system is expected to be up and running within the next 1-7 days. We apologise for any issues with the website, while it is being updated. This is a major project and will not be rushed.";
+
+function SpleduLoginNotify()
+{
+	alert(message)
+}
